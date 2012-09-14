46de074a20f2a94f3eaac73a84c67988c3e7a267
Marc Beaupre <jack.quack@gmail.com>
2012-09-14 11:32:22 -0400

    added callbacks to store asset worker

--- lib/backgrounder/workers/store_asset.rb
+++ lib/backgrounder/workers/store_asset.rb
@@ -16,18 +16,18 @@ module CarrierWave
         errors << ::ActiveRecord::RecordNotFound      if defined?(::ActiveRecord)
         errors << ::Mongoid::Errors::DocumentNotFound if defined?(::Mongoid)
 
-        record = begin
+        @record = begin
           constantized_resource.find(id)
         rescue *errors
           nil
         end
 
-        if record && record.send(:"#{column}_tmp")
-          store_directories(record)
-          record.send :"process_#{column}_upload=", true
-          record.send :"#{column}_tmp=", nil
-          File.open(cache_path) { |f| record.send :"#{column}=", f }
-          if record.save!
+        if @record && @record.send(:"#{column}_tmp")
+          store_directories(@record)
+          @record.send :"process_#{column}_upload=", true
+          @record.send :"#{column}_tmp=", nil
+          File.open(cache_path) { |f| @record.send :"#{column}=", f }
+          if @record.save!
             FileUtils.rm_r(tmp_directory, :force => true)
           end
         end
@@ -39,6 +39,7 @@ module CarrierWave
         self.klass, self.id, self.column = klass, id, column
       end
 
+
       def constantized_resource
         klass.is_a?(String) ? klass.constantize : klass
       end
@@ -49,6 +50,43 @@ module CarrierWave
         @cache_path      = File.join(cache_directory, asset_tmp)
         @tmp_directory   = File.join(cache_directory, asset_tmp.split("/").first)
       end
+
+      def enqueue(job)
+        if @record.respond_to?(:enqueue_callback)
+          @record.enqueue_callback(job)
+        end
+      end
+
+      def before(job)
+        if @record.respond_to?(:before_callback)
+          @record.before_callback(job)
+        end
+      end
+
+      def after(job)
+        if @record.respond_to?(:after_callback)
+          @record.after_callback(job)
+        end
+      end
+
+      def success(job)
+        if @record.respond_to?(:success_callback)
+          @record.success_callback(job)
+        end
+      end
+
+      def error(job, exception)
+        if @record.respond_to?(:error_callback)
+          @record.error_callback(job, exception)
+        end
+      end
+
+      def failure
+        if @record.respond_to?(:failure_callback)
+          @record.failure_callback
+        end
+      end
+
     end # StoreAsset
 
   end # Workers
